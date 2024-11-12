1396fef5413b5e8735490f7d313e2e1ef7ac166c
mikejbp <migueljosebp@gmail.com>
2024-11-12 18:56:19 -0400

    [feat]: Add routing with Wouter and create Home and Login pages
    Adding barrels to simplify use of imports

---
+++ src/app/routes/AppRouter.tsx
@@ -0,0 +1,12 @@
+import { Route } from "wouter"
+import { Home, Login } from "../../pages"
+
+export default function AppRouter() {
+  return (
+    <main>
+      <Route path="/" component={Home} />
+
+      <Route path="/login" component={Login} />
+    </main>
+  )
+}
