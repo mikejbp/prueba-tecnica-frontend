296b2a68cfd470905d248c6e0e27dca2a71b8a0a
mikejbp <migueljosebp@gmail.com>
2024-11-14 19:11:58 -0400

    feat: Enhance MakeFetch component with background gradient and shadow for improved aesthetics

--- src/app/components/home/MakeFetch.tsx
+++ src/app/components/home/MakeFetch.tsx
@@ -1,7 +1,14 @@
 export function MakeFetch() {
   return (
-    <section id="makefetch" className="max-w-screen-xl mx-auto my-20 px-5 sm:px-10 ">
-      <div className="bg-blue rounded-2xl p-10 flex flex-col max-h-96 sm:flex-row ">
+    <section id="makefetch" className="max-w-screen-xl mx-auto my-20 px-5 sm:px-10">
+      <div
+        className="bg-blue rounded-2xl p-10 flex flex-col max-h-96 sm:flex-row shadow-md"
+        style={{
+          backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 700 100"><defs><linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%"><stop offset="0%" style="stop-color:rgb(68, 135, 255);;stop-opacity:1" /><stop offset="40%" style="stop-color:rgb(62, 127, 249);stop-opacity:1" /></linearGradient></defs><circle cx="40" cy="50" r="60" fill="url(%23grad1)" /><circle cx="100" cy="50" r="60" fill="url(%23grad1)" /><circle cx="140" cy="50" r="60" fill="url(%23grad1)" /><circle cx="180" cy="50" r="60" fill="url(%23grad1)" /><circle cx="220" cy="50" r="60" fill="url(%23grad1)" /><circle cx="260" cy="50" r="60" fill="url(%23grad1)" /><circle cx="300" cy="50" r="60" fill="url(%23grad1)" /></svg>')`,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'repeat-x',
+        }}
+      >
         <aside className="hidden sm:block mt-10">
           <div className="rounded-full bg-orangeYellow-dark h-[12rem] w-[12rem] lg:h-[16rem] lg:w-[16rem] relative">
             {/* Background image */}
