670a46ba1c61955d3d2099b0ca7b61f3fe75df47
mikejbp <migueljosebp@gmail.com>
2024-11-14 20:25:31 -0400

    feat: Add BlueWaves and RedWaves components for enhanced visual effects; update Home page to include new components

--- src/app/components/home/DynamicSection.tsx
+++ src/app/components/home/DynamicSection.tsx
@@ -1,4 +1,4 @@
-import { CustomInput, CustomSquare } from '..';
+import { BlueWaves, CustomInput, CustomSquare } from '..';
 import { useSectionStore } from '../../../store/store';
 import { sectionData } from '../../helpers';
 
@@ -8,31 +8,35 @@ export function DynamicSection() {
   const currentSection = sectionData.find((section) => section.id === activeSection);
 
   return (
-    <section
-      id={currentSection?.id ?? ''}
-      className="mx-auto flex flex-col gap-y-20 lg:flex-row justify-around items-center xl:items-start px-5 sm:px-10 xl:px-0 max-w-screen-2xl bg-transparent mt-20 xl:mt-64 mb-20"
-    >
-      <aside className="flex flex-col gap-8 px-5 md:px-10 xl:p-x0 relative">
-        <h2 className="text-5xl font-bold text-red-light font-comfortaa">
-          {currentSection?.title ?? ''}
-        </h2>
-        <p className="text-description text-gray max-w-[25rem] leading-7 font-openSans ">
-          {currentSection?.description ?? ''}
-        </p>
-        <span className="flex items-center gap-6 leading-6 text-red-light text-description font-bold font-segoe">
-          {currentSection?.additionalText ?? ''}
-        </span>
+    <div className="relative">
+      <section
+        id={currentSection?.id ?? ''}
+        className="z-10 mx-auto flex flex-col gap-y-20 lg:flex-row justify-around items-center xl:items-start px-5 sm:px-10 xl:px-0 max-w-screen-2xl bg-transparent mt-20 xl:mt-64 mb-20"
+      >
+        <aside className="flex flex-col gap-8 px-5 md:px-10 xl:p-x0 relative z-20">
+          <h2 className="text-5xl font-bold text-red-light font-comfortaa">
+            {currentSection?.title ?? ''}
+          </h2>
+          <p className="text-description text-gray max-w-[25rem] leading-7 font-openSans ">
+            {currentSection?.description ?? ''}
+          </p>
+          <span className="flex items-center gap-6 leading-6 text-red-light text-description font-bold font-segoe">
+            {currentSection?.additionalText ?? ''}
+          </span>
 
-        <div className="flex items-center gap-6 mt-10">
-          <CustomInput placeholder="Zip Code" type="number" width="w-60" />
-        </div>
-      </aside>
+          <div className="flex items-center gap-6 mt-10">
+            <CustomInput placeholder="Zip Code" type="number" width="w-60" />
+          </div>
+        </aside>
 
-      <aside className="grid grid-cols-2 items-stretch justify-center gap-y-8 gap-x-10">
-        {currentSection?.services?.map((service, index) => (
-          <CustomSquare key={index} image={service.image} alt={service.alt} />
-        ))}
-      </aside>
-    </section>
+        <aside className="grid grid-cols-2 items-stretch justify-center gap-y-8 gap-x-10 relative z-20">
+          {currentSection?.services?.map((service, index) => (
+            <CustomSquare key={index} image={service.image} alt={service.alt} />
+          ))}
+        </aside>
+      </section>
+
+      <BlueWaves />
+    </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,58 +3,15 @@ import {
   HowItWorks,
   MainSection,
   MakeFetch,
+  RedWaves,
   Testimonials,
-  Wave,
 } from '../app/components';
 import HomeLayout from '../app/components/layout/HomeLayout';
 
 export function Home() {
   return (
     <HomeLayout>
-      <div className="hidden xl:block">
-        <Wave
-          id="wave1"
-          style={{
-            transform: 'rotate(180deg)',
-            transition: '0.3s',
-            top: '-30px',
-            position: 'absolute',
-            width: '100%',
-            height: 'auto',
-            zIndex: -1,
-          }}
-          viewBox="0 15 1300 490"
-          opacity={1}
-        />
-        <Wave
-          id="wave2"
-          style={{
-            transform: 'rotate(180deg)',
-            transition: '0.3s',
-            top: '0',
-            position: 'absolute',
-            width: '100%',
-            height: 'auto',
-            zIndex: -2,
-          }}
-          viewBox="0 60 1450 490"
-          opacity={0.85}
-        />
-        <Wave
-          id="wave3"
-          style={{
-            transform: 'rotate(180deg)',
-            transition: '0.3s',
-            top: '0',
-            position: 'absolute',
-            width: '100%',
-            height: 'auto',
-            zIndex: -3,
-          }}
-          viewBox="200 80 1300 490"
-          opacity={0.8}
-        />
-      </div>
+      <RedWaves />
       <MainSection />
       <DynamicSection />
       <Testimonials />
